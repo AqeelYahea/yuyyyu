ddb6831166aa5c96704bcf150d2ab47e7a9167a4
AqeelYahea <108987295+AqeelYahea@users.noreply.github.com>
2022-07-10 02:37:07 +0300

    Delete projectAqeel directory

--- projectAqeel/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace projectAqeel.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
